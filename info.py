957c40e7fafb754ea56d95832c0a0a0bc0660cc9
Chen, Po Cheng <pchen432@gatech.edu>
2024-11-14 17:03:22 -0500

    Initial commit

---
+++ info.py
@@ -0,0 +1,76 @@
+
+#This File will contain the information to be displayed in your portfolio
+
+#CHANGE BELOW
+profile_picture = "Images/profile.jpeg"
+about_me = "I'm SpongeBobe Square Pants. I work at the Krusty Krab and I make fantastic Krabby Patties. "
+
+
+#CHANGE BELOW (OPTIONAL)
+linkedin_image_url = "https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg"
+github_image_url = "https://cdn-icons-png.flaticon.com/256/25/25231.png"
+email_image_url = "https://logowik.com/content/uploads/images/513_email.jpg"
+
+#CHANGE BELOW
+my_linkedin_url = "https://www.linkedin.com/in/spongebob-squarepants-44b857281/"
+my_github_url = "https://github.com/spongebob-squarepants"
+my_email_address = "spongebob@gmail.com"
+
+
+education_data ={
+    'Degree': 'Bachelor of Science in Computer Science',
+    'Institution': 'Georgia Institute of Technology',
+    'Location': 'Atlanta, GA',
+    'Graduation Date': 'Never',
+    'GPA': '4.0'
+}
+course_data = {
+    "code":["CS 1301", "CS 1331", "MATH 1554", "CS 2050"], 
+    "names":["Intro to CS", "Intro to OOP", "Linear Algebra", "Discrete Mathematics"], 
+    "semester_taken":["2nd", "3rd", "2nd", "2nd"],
+    "skills":["Python is not my favorite snake anymore", "OOP(S!) (Java Edition)", "Basically row reduction", "If only my grade was discrete too..."],
+    }
+experience_data = {
+    "Cook at Krusty Krab": (["- Worked on designing the Krabby Patty Recipe",
+                                                                          "- Increased Company Revenue by 200 dollars", "- Made Mr. Krabs Proud"],"Images/cook.jpg"),
+    "Cleaner at Krusty Krab":(["- Made the floor almost reflective",
+                                                           "- Increased customer visits by three folds by making restaurant clean"],"Images/cleaner.jpg"),
+    "Jelly Fish Catcher":(["- Caught 100+ Jellyfish with the help of my colleague Patrick"],"Images/jelly.jpg")
+
+}
+
+projects_data = {
+    "Krabby Patty Recipe": "Created the famous Recipe and tailored it from scratch",
+}
+
+programming_data = {
+    "Python": 90,
+    "Java": 70,
+    "C": 40,
+}
+
+#CHANGE BELOW (OPTIONAL)
+programming_icons = {
+    "Python": "🐍",
+    "Java": "☕",
+    "C": "🔍",
+}
+spoken_icons = {"French": "🇫🇷",
+    "English": "🇬🇧",
+    "Spanish":"🇪🇸"
+}
+
+#CHANGE BELOW
+spoken_data = {
+    "English": "Fluent",
+    "French": "Fluent",
+    "Spanish": "Fluent",
+}
+leadership_data = {
+    "Driver at Mrs Puff's School": (["- Elected Worst Student of the Century"],"Images/puff.jpg"),
+
+}
+activity_data={
+    "SpongeBob and Patrick Friendship Club": ["- Conviced Squidwards to have fun", 
+            "- Sat under Patrick's rock"]
+}
