754dce45d3d24cb52bc26e3e5351f36e9ef0381d
Chenry <henrychen0214@gmail.com>
2024-11-15 19:13:13 -0500

    asdf

--- henryInfo.py
+++ henryInfo.py
@@ -1,7 +1,7 @@
 #This File will contain the information to be displayed in your portfolio
 
 #CHANGE BELOW
-profile_picture = "Images/profile.jpeg"
+profile_picture = "Images/yae miko square 512 512.png"
 about_me = "I'm SpongeBobe Square Pants. I work at the Krusty Krab and I make fantastic Krabby Patties. "
 
 
@@ -11,41 +11,40 @@ github_image_url = "https://cdn-icons-png.flaticon.com/256/25/25231.png"
 email_image_url = "https://logowik.com/content/uploads/images/513_email.jpg"
 
 #CHANGE BELOW
-my_linkedin_url = "https://www.linkedin.com/in/spongebob-squarepants-44b857281/"
-my_github_url = "https://github.com/spongebob-squarepants"
-my_email_address = "spongebob@gmail.com"
+my_linkedin_url = "https://www.linkedin.com/in/henry-chen-980s"
+my_github_url = "https://github.com/uwuChenry"
+my_email_address = "pochengchen@gatech.edu"
 
 
 education_data ={
-    'Degree': 'Bachelor of Science in Computer Science',
+    'Degree': 'Bachelor of Science in Computer Engineering',
     'Institution': 'Georgia Institute of Technology',
     'Location': 'Atlanta, GA',
     'Graduation Date': 'Never',
     'GPA': '4.0'
 }
 course_data = {
-    "code":["CS 1301", "CS 1331", "MATH 1554", "CS 2050"], 
-    "names":["Intro to CS", "Intro to OOP", "Linear Algebra", "Discrete Mathematics"], 
-    "semester_taken":["2nd", "3rd", "2nd", "2nd"],
-    "skills":["Python is not my favorite snake anymore", "OOP(S!) (Java Edition)", "Basically row reduction", "If only my grade was discrete too..."],
+    "code":["CS 1301", "MATH 1552", "MATH 1554", "ENG 1102", "INTA 1200"], 
+    "names":["Intro to CS", "Calculus II", "Linear Algebra", "English Composition II", "American Government"], 
+    "semester_taken":["1st", "1st", "1st", "1st", "1st"],
+    "skills":["Python is not my favorite snake anymore", "Integration and Series", "Basically row reduction", "I write English", "I hate politics"],
     }
 experience_data = {
-    "Cook at Krusty Krab": (["- Worked on designing the Krabby Patty Recipe",
-                                                                          "- Increased Company Revenue by 200 dollars", "- Made Mr. Krabs Proud"],"Images/cook.jpg"),
-    "Cleaner at Krusty Krab":(["- Made the floor almost reflective",
-                                                           "- Increased customer visits by three folds by making restaurant clean"],"Images/cleaner.jpg"),
-    "Jelly Fish Catcher":(["- Caught 100+ Jellyfish with the help of my colleague Patrick"],"Images/jelly.jpg")
-
+    "Skywalker Robotics Club | Founder": (["- Established a community-based club for motivated teenagers without access to robotics in school in New Zealand.",
+                            "- Hosted 7 outreach workshops with 1000+ attendees to promote STEM Education in New Zealand.", 
+                            "- Taught 650+ hrs of CAD & programming, provided troubleshooting consultation and competition tips to teams in New Zealand, Taiwan, Australia and United States both in person and online.",
+                            "- Mentored 10+ teams, leading to a score increase of 150% and qualifying to the Vex World Championship 6 times"],"Images/cook.jpg"),
 }
 
 projects_data = {
+    "Drone Assisted Water Sampling": "Developed and 3D-printed a custom quadcopter equipped with a retractable water collection payload, designed to sample water at targeted depths and specified GPS locations.  Achieved up to 50% reduction in water sampling time compared to conventional methods such as boats and kayaks.	Awarded Honorable mention at the Taiwan Macronix Science Fair.",
     "Krabby Patty Recipe": "Created the famous Recipe and tailored it from scratch",
 }
 
 programming_data = {
     "Python": 90,
     "Java": 70,
-    "C": 40,
+    "C/C++": 90,
 }
 
 #CHANGE BELOW (OPTIONAL)
@@ -54,16 +53,19 @@ programming_icons = {
     "Java": "☕",
     "C": "🔍",
 }
-spoken_icons = {"French": "🇫🇷",
+spoken_icons = {
+    "French": "🇫🇷",
     "English": "🇬🇧",
-    "Spanish":"🇪🇸"
+    "Spanish":"🇪🇸",
+    "Taiwanese": "🇹🇼",
+    "Chinese": "㊗️"
 }
 
 #CHANGE BELOW
 spoken_data = {
-    "English": "Fluent",
-    "French": "Fluent",
-    "Spanish": "Fluent",
+    "English": "Native",
+    "Chinese": "Native",
+    "Taiwanese": "Fluent",
 }
 leadership_data = {
     "Driver at Mrs Puff's School": (["- Elected Worst Student of the Century"],"Images/puff.jpg"),
